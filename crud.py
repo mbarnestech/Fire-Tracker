58f39a61a8a86fe1ba1c33064f6f7ef61d0c078b
mbarnestech <maggie.barnes@gmail.com>
2023-04-04 16:02:16 -0600

    add comments and clean up create_decimal_latlong function

--- crud.py
+++ crud.py
@@ -9,21 +9,32 @@ from model import Trail, TrailPoint, Fire, connect_to_db, db
 # helper function
 
 def create_decimal_latlong(latlong):
+    """change coordinates from sexagesimal string format to decimal float format"""
+
+    # remove whitespace before and after
     latlong.strip()
+    # create string to move degrees to
     decimal_string = ''
+    # set assumption of positive lat long
     neg = False
+    # if lat long is negative
     if latlong[0] == '-':
+        # set negative equal to true
         neg = True
+        # remove first char, '-', from latlong string
         latlong = latlong[1:]
-        print(f'check 1: {neg=}, {decimal_string=}, {latlong=}')
+    # run while loop for first 2 or 3 numbers
     while latlong[0].isnumeric():
+        # add the number to the decimal string
         decimal_string += latlong[0]
+        # remove the number from the latlong string
         latlong = latlong[1:]
-        print(f'checks 2-4: {decimal_string=}, {latlong=}')
+    # remove whitespace and . from latlong between degrees and minutes 
     while not latlong[0].isnumeric():
         latlong = latlong[1:]
-        print(f'checks: {decimal_string=}, {latlong=}')
+    # change the decimal_string to a float
     decimal_latlong = float(decimal_string)
+    # 
     decimal_latlong += float(latlong[:2]) / 60
     latlong = latlong[2:]
     print(f'more checks: {decimal_string=}, {latlong=}')
@@ -31,10 +42,6 @@ def create_decimal_latlong(latlong):
     if neg == True:
         decimal_latlong *= -1
     return decimal_latlong
-
-
-        
-
 
 
 # def create_trail(name, trail_url, gpx_url, miles, high_elevation, low_elevation, elevation_gain, trail_type, difficulty, last_condition, last_condition_update, last_condition_notes, stars, votes, state, area, subarea, city, dogs):
@@ -53,7 +60,7 @@ def create_fire(fire_url, fire_name, latitude, longitude, incident_type, last_up
 
     decimal_latitude = create_decimal_latlong(latitude)
     decimal_longitude = create_decimal_latlong(longitude)
-    
+
     return Fire(fire_url=fire_url, fire_name=fire_name, latitude=decimal_latitude, longitude=decimal_longitude, incident_type=incident_type, last_updated=last_updated, size=size, contained=contained)
 
 
