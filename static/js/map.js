064be17a8327e621202f0f79442c3e22380d2102
mbarnestech <maggie.barnes@gmail.com>
2023-04-06 21:44:40 -0600

    start on map js file

---
+++ static/js/map.js
@@ -0,0 +1,18 @@
+// enforce better formatting
+'use strict';
+
+function fetchTrailPoints{
+  fetch('/go_to_map')
+    .then((response)=>response.json())
+    .then((data)=>{
+      // TODO
+    });
+}
+
+function initMap() {
+  let uluru = {lat: -25.344, lng: 131.036};
+  let map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru}
+  );
+  let marker = new google.maps.Marker({position: uluru, map: map});
+}
