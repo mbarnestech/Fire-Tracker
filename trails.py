24bbb99736978742c3c8451283a92096f0829eb3
mbarnestech <maggie.barnes@gmail.com>
2023-04-13 16:57:18 -0600

    scraper file for hiking project page; functional but will not be moving forward with this.

--- trails.py
+++ trails.py
@@ -3,10 +3,12 @@
 # import Python modules
 from bs4 import BeautifulSoup
 import requests
+import sys
+import re
 
-# import local modules
-from seed import seed_trail
-
+#  temp for test file
+# with open('./testcode/testscrape.html') as file:
+#    soup = BeautifulSoup(file, 'html.parser')
 
 # currently designing this to be run interactively
 def get_soup(website):
@@ -15,9 +17,9 @@ def get_soup(website):
     return BeautifulSoup(response.text, 'html.parser')
 
 # option to get website from command line input - will need python3 trails.py <url for website>
-# import sys
-# url = sys.argv[1]
-# soup = get_soup(url)
+# if sys.argv[1]:
+#     url = sys.argv[1]
+#     soup = get_soup(url)
 
 def get_trails(soup):
     """create trail list for seeding"""
@@ -27,10 +29,19 @@ def get_trails(soup):
     
     # currently planning on running this by subarea to state, area same for all trails
     area = soup.h1.get_text()
-    table_data = soup.find_all(class_='trail-row')
-    # list I need:
-    {'trail_name': 'Cochise Stronghold Trail #279', 
-        'hp_id': '7030929', 
-        'state': 'AZ',
-        'area': 'Southern Arizona and Tucson',
-        'city': 'Saint David'},
+    data_by_row = soup.select('.trail-row')
+    for row in data_by_row:
+        trail_name = row.strong.get_text() # gets string of trail name
+        hp_id = re.search(r'\d{7}', str(row.a)).group(0) # gets string of hiking project id number
+        city, state = row.find_all('td')[-3].get_text()[1:-1].split(', ') # gets string in 'City, State' format
+        trails.append({
+            'trail_name': trail_name, 
+            'hp_id': hp_id, 
+            'state': state,
+            'area': area,
+            'city': city
+        })
+    return trails
+
+# trails = get_trails(soup)
+
