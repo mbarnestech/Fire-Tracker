0e30505294214a9a56cb62e7fb0404308b3da4ad
mbarnestech <maggie.barnes@gmail.com>
2023-04-13 19:00:02 -0600

    move SQLAlchemy query functions from crud.py to new file query.py

--- crud.py
+++ crud.py
@@ -1,12 +1,12 @@
-# import Python modules
-from datetime import datetime
+"""CRUD functions live here"""
 
 # import local modules
-from model import Region, Forest, District, Trail, TrailPoint, Fire, connect_to_db, db
+from model import Region, Forest, District, Trail, TrailPoint, Fire, connect_to_db
 import helper
 
 #---------------------------------------------------------------------#
-# functions to create class instances
+# CREATE
+
 def create_region(region_id, region_name):
     """Create and return a new region."""
     return Region(region_id=region_id, region_name=region_name)
@@ -39,30 +39,6 @@ def create_fire(fire_url, fire_name, latitude, longitude, incident_type, last_up
 
 #---------------------------------------------------------------------#
 
-def get_trail_with_trail_name(trail_name):
-    """return single Trail instance with trail_name attribute"""
-    return Trail.query.filter_by(trail_name = trail_name).one()
-
-
-def get_trailpoint_list_with_trail_id(trail_id):
-    """return a list of TrailPoint objects corresponding to a trail id"""
-    return db.session.query(TrailPoint).filter(TrailPoint.trail_id == trail_id).all()
-
-def get_fires():
-    """return a list of all Fire objects"""
-    return Fire.query.all()
-
-def get_trails():
-    """return a list of all Trail objects"""
-    return Trail.query.all()
-
-def get_fires_with_fire_ids(fires):
-    """get list of Fire objects corresponding to list of fire_id numbers"""
-    fire_list = []
-    for fire in fires:
-        fire_list.append(Fire.query.filter(Fire.fire_id == fire.fire_id).one())
-    return fire_list
-    
 # Connect to database when running crud.py interactively
 if __name__ == '__main__':
     from server import app
